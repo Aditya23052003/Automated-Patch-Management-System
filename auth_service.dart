49c6e458ba3f002ff525672b87218cd6be9efa15
Aditya Jadhav <98929144+Aditya23052003@users.noreply.github.com>
2025-05-16 17:25:58 +0530

    Create auth_service.dart

---
+++ auth_service.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> login(String email, String password) async {
+    try {
+      UserCredential userCred = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return userCred.user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<User?> register(String email, String password) async {
+    try {
+      UserCredential userCred = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return userCred.user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  void logout() async {
+    await _auth.signOut();
+  }
+
+  User? get currentUser => _auth.currentUser;
+}
